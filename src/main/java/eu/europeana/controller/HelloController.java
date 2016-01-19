e75abc74de57834752bf7784fe22d368b6bf58bd
a1300613 <a1300613@unet.univie.ac.at>
2016-01-20 00:49:11 +0100

    changed hello.jsp in home.jsp and made a default routing / to home.jsp

--- src/main/java/eu/europeana/controller/HelloController.java
+++ src/main/java/eu/europeana/controller/HelloController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/greeting")
+	@RequestMapping(value="/index.html")
 	public String sayHello(Model model) {
 		
 		//model.addAttribute("greeting", "Hello Peanuts");
 		
-		return "hello"; //return the view hello.jsp to the user
+		return "home"; //return the view hello.jsp to the user
 	}
 }
