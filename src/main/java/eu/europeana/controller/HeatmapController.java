ee18c73d69a01ee08501af4878390f31c83ca1e6
a1300613 <a1300613@unet.univie.ac.at>
2016-01-20 01:27:11 +0100

    Added HeatmapController and heatmap.jsp

---
+++ src/main/java/eu/europeana/controller/HeatmapController.java
@@ -0,0 +1,15 @@
+package eu.europeana.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HeatmapController {
+
+	@RequestMapping(value="/heatmap")
+	public String callHeatmap() {
+		
+		
+		return "heatmap"; // heatmap.jsp
+	}
+}
