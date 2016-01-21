5e934d6b14d5032b2ecc15e3e862f5a07ed9d125
a1300 <a1300613@unet.univie.ac.at>
2016-01-21 02:43:16 +0100

    Added to HomeController RequestMappings for index.html / and default.html

--- src/main/java/eu/europeana/controller/HomeController.java
+++ src/main/java/eu/europeana/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-	@RequestMapping(value="/index.html")
+	@RequestMapping(value={"/index.html", "/", "default.html"})
 	public String comeHome(Model model) {
 		
 		//model.addAttribute("greeting", "Hello Peanuts");
