bc1d7e6dfd563c44a0510a1e66ac2787118bc074
RicardoAranguena <univ.ricardo.aranguena@gmail.com>
2016-01-21 02:07:49 +0100

    little comment
    Change-Id: I1ac92219d5ac37b0885e8dc28348b53580516c48

--- src/main/java/eu/europeana/controller/SearchController.java
+++ src/main/java/eu/europeana/controller/SearchController.java
@@ -1,4 +1,4 @@
-package eu.europeana.controller;
+ package eu.europeana.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SearchController {
 	
-	@RequestMapping(value="/search.html")
+	@RequestMapping(value="/search.html")//how we tight this to the url
 	public String search() {
 		
 		return "search";
