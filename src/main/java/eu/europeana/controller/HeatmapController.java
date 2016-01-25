2f8a57a3ca6ca64f8a0824d28f96ecb604a98b3a
a1300 <a1300613@unet.univie.ac.at>
2016-01-25 17:37:54 +0100

    Prepared HeatmapController for work

--- src/main/java/eu/europeana/controller/HeatmapController.java
+++ src/main/java/eu/europeana/controller/HeatmapController.java
@@ -5,19 +5,37 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import eu.europeana.model.LongLat;
 
 @Controller
 public class HeatmapController {
 
-	@RequestMapping(value="/heatmap")
-	public ModelAndView callHeatmap() {
-		
-		
-		
+	@RequestMapping(value="/heatmap", method=RequestMethod.GET)
+	public ModelAndView getHeatmap() {
 		
 		ModelAndView mav = new ModelAndView("heatmap");
+		
+		mav.addObject("lists", returnDummyValues() );
+		
+		return mav;
+	}
+	
+	@RequestMapping(value="/heatmap", method=RequestMethod.POST)
+	public ModelAndView postHeatmap() {
+		
+		ModelAndView mav = new ModelAndView("heatmap");
+		
+		mav.addObject("lists", returnDummyValues() );
+		
+		return mav;
+	}
+	
+	
+	
+	public List<LongLat> returnDummyValues() {
+		
 		List<LongLat> list = new ArrayList<LongLat>();
 		list.add(new LongLat(52.5, 13.4, 1)); //test values:
 		list.add(new LongLat(52.34, 13.5, 1));
@@ -25,8 +43,7 @@ public class HeatmapController {
 		list.add(new LongLat(52.15, 13.7, 1));
 		list.add(new LongLat(52.34, 13.8, 1));
 		list.add(new LongLat(52.1, 13.9, 1));
-		mav.addObject("lists", list);
 		
-		return mav;
+		return list;
 	}
 }
